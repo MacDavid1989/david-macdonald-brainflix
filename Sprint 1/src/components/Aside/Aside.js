84b8025f0414be757c79f0a036eba19d889f9433
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-03 23:18:02 -0500

    Add sideVideo array to Aside component. Map sideVideo array to render sideVideo components.

--- Sprint 1/src/components/Aside/Aside.js
+++ Sprint 1/src/components/Aside/Aside.js
@@ -1,63 +1,73 @@
+const sideVideo = [
+    {
+        id: '1',
+        title: 'type of <string>',
+        channel: 'type of <string>',
+        image: 'type of <string>'
+    },
+    {
+        id: '2',
+        title: 'Become A Travel Pro In One Easy Lesson…',
+        channel: 'Scotty Cranmer',
+        image: 'type of <string>'
+    },
+    {
+        id: '3',
+        title: 'Les Houches The Hidden Gem Of The…',
+        channel: 'Scotty Cranmer',
+        image: 'type of <string>'
+    },
+    {
+        id: '4',
+        title: 'Travel Health Useful Medical Information…',
+        channel: 'Scotty Cranmer',
+        image: 'type of <string>'
+    },
+    {
+        id: '5',
+        title: 'Cheap Airline Tickets Great Ways To Save',
+        channel: 'Emily Harper',
+        image: 'type of <string>'
+    },
+    {
+        id: '6',
+        title: 'Take A Romantic Break In A Boutique Hotel',
+        channel: 'Ethan Owen',
+        image: 'type of <string>'
+    },
+    {
+        id: '7',
+        title: 'Choose The Perfect Accommodations',
+        channel: 'Lydia Perez',
+        image: 'type of <string>'
+    },
+    {
+        id: '8',
+        title: 'Cruising Destination Ideas',
+        channel: 'Timothy Austin',
+        image: 'type of <string>'
+    },
+    {
+        id: '9',
+        title: 'Train Travel On Track For Safety',
+        channel: 'Scotty Cranmer',
+        image: 'type of <string>'
+    },
+];
+
 function Aside() {
     return (
         <aside>
             <h3>NEXT VIDEO</h3>
-            <div>
-                <img />
+            {sideVideo.map(video =>
                 <div>
-                    <h4>Become A Travel Pro In One Easy Lesson…</h4>
-                    <h5>Scotty Cranmer</h5>
+                    <img />
+                    <div>
+                        <h4>{video.title}</h4>
+                        <h5>{video.channel}</h5>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <img />
-                <div>
-                    <h4>Les Houches The Hidden Gem Of The…</h4>
-                    <h5>Scotty Cranmer</h5>
-                </div>
-            </div>
-            <div>
-                <img />
-                <div>
-                    <h4>Travel Health Useful Medical Information…</h4>
-                    <h5>Scotty Cranmer</h5>
-                </div>
-            </div>
-            <div>
-                <img />
-                <div>
-                    <h4>Cheap Airline Tickets Great Ways To Save</h4>
-                    <h5>Emily Harper</h5>
-                </div>
-            </div>
-            <div>
-                <img />
-                <div>
-                    <h4>Take A Romantic Break In A Boutique Hotel</h4>
-                    <h5>Ethan Owen</h5>
-                </div>
-            </div>
-            <div>
-                <img />
-                <div>
-                    <h4>Choose The Perfect Accommodations</h4>
-                    <h5>Lydia Perez</h5>
-                </div>
-            </div>
-            <div>
-                <img />
-                <div>
-                    <h4>Cruising Destination Ideas</h4>
-                    <h5>Timothy Austin</h5>
-                </div>
-            </div>
-            <div>
-                <img />
-                <div>
-                    <h4>Train Travel On Track For Safety</h4>
-                    <h5>Scotty Cranmer</h5>
-                </div>
-            </div>
+            )}
         </aside>
     )
 }
