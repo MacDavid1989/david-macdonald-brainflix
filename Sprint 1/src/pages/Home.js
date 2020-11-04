766767a3f6d9d4427401148de584f45e139249ef
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-03 22:13:18 -0500

    Add HTML components to Home.js to set intial full page structure. Include some context to help visualize page.

--- Sprint 1/src/pages/Home.js
+++ Sprint 1/src/pages/Home.js
@@ -1,9 +1,180 @@
 function Home() {
 
     return (
-        <div className="App">
-            Hello World
-        </div>
+        <>
+            <header>
+                <section className="search-bar">
+                    <a>
+                        <svg></svg>
+                    </a>
+                    <form>
+                        <input value="Search"></input>
+                        <div>
+                            <button>UPLOAD</button>
+                            <svg></svg>
+                        </div>
+                    </form>
+                </section>
+                <section className="hero">
+                    <video>
+                        <div>
+                            <svg></svg>
+                            <progress></progress>
+                            <div>
+                                <svg></svg>
+                                <svg></svg>
+                            </div>
+                        </div>
+                    </video>
+                </section>
+            </header>
+            <main>
+                <article>
+                    <h1>BMX Rampage: 2018 Highlights</h1>
+                    <div>
+                        <div>
+                            <h2>By Red Cow</h2>
+                            <h5>12/18/2018</h5>
+                        </div>
+                        <div>
+                            <span>1,001,023</span>
+                            <span>110,985</span>
+                        </div>
+                    </div>
+                    <p>
+                        On a gusty day in Southern Utah, a group of 25
+                        daring mountain bikers blew the doors off what is
+                        possible on two wheels, unleashing some of the
+                        biggest moments the sport has ever seen. While
+                        mother nature only allowed for one full run before
+                        the conditions made it impossible to ride, that was
+                        all that was needed for event veteran Kyle Strait,
+                        who won the event for the second time -- eight years
+                        after his first Red Cow Rampage title
+                    </p>
+                </article>
+                <section className="comments">
+                    <h2>3 Comments</h2>
+                    <form>
+                        <img />
+                        <div>
+                            <div>
+                                <label>JOIN THE CONVERSATION</label>
+                                <textarea value="Write comment here"></textarea>
+                            </div>
+                            <button>COMMENT</button>
+                        </div>
+                    </form>
+                    <div>
+                        <img />
+                        <div>
+                            <div>
+                                <h4>Micheal Lyons</h4>
+                                <h5>12/18/2018</h5>
+                            </div>
+                            <p>
+                                They BLEW the ROOF off at their
+                                last show, once everyone started
+                                figuring out they were going. This is
+                                still simply the greatest opening of a
+                                concert I have EVER witnessed.
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <img />
+                        <div>
+                            <div>
+                                <h4>Gary Wong</h4>
+                                <h5>12/18/2018</h5>
+                            </div>
+                            <p>
+                                Every time I see him shred I feel so
+                                motivated to get off my couch and
+                                hop on my board. He’s so talented! I
+                                wish I can ride like him one day so I
+                                can really enjoy myself!
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <img />
+                        <div>
+                            <div>
+                                <h4>Theodore Duncan</h4>
+                                <h5>12/18/2018</h5>
+                            </div>
+                            <p>
+                                How can someone be so good!!!
+                                You can tell he lives for this and
+                                loves to do it every day. Everytime I
+                                see him I feel instantly happy! He’s
+                                definitely my favorite ever!
+                            </p>
+                        </div>
+                    </div>
+                </section>
+            </main>
+            <aside>
+                <h3>NEXT VIDEO</h3>
+                <div>
+                    <img />
+                    <div>
+                        <h4>Become A Travel Pro In One Easy Lesson…</h4>
+                        <h5>Scotty Cranmer</h5>
+                    </div>
+                </div>
+                <div>
+                    <img />
+                    <div>
+                        <h4>Les Houches The Hidden Gem Of The…</h4>
+                        <h5>Scotty Cranmer</h5>
+                    </div>
+                </div>
+                <div>
+                    <img />
+                    <div>
+                        <h4>Travel Health Useful Medical Information…</h4>
+                        <h5>Scotty Cranmer</h5>
+                    </div>
+                </div>
+                <div>
+                    <img />
+                    <div>
+                        <h4>Cheap Airline Tickets Great Ways To Save</h4>
+                        <h5>Emily Harper</h5>
+                    </div>
+                </div>
+                <div>
+                    <img />
+                    <div>
+                        <h4>Take A Romantic Break In A Boutique Hotel</h4>
+                        <h5>Ethan Owen</h5>
+                    </div>
+                </div>
+                <div>
+                    <img />
+                    <div>
+                        <h4>Choose The Perfect Accommodations</h4>
+                        <h5>Lydia Perez</h5>
+                    </div>
+                </div>
+                <div>
+                    <img />
+                    <div>
+                        <h4>Cruising Destination Ideas</h4>
+                        <h5>Timothy Austin</h5>
+                    </div>
+                </div>
+                <div>
+                    <img />
+                    <div>
+                        <h4>Train Travel On Track For Safety</h4>
+                        <h5>Scotty Cranmer</h5>
+                    </div>
+                </div>
+            </aside>
+        </>
     );
 }
 
