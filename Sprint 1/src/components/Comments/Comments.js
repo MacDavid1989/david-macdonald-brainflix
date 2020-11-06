45c8a29f3502303234594cefc00110606a77b8a6
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-05 20:20:38 -0500

    Update classes in Comments component. Change heading labels to span.

--- Sprint 1/src/components/Comments/Comments.js
+++ Sprint 1/src/components/Comments/Comments.js
@@ -1,26 +1,26 @@
-import Mohan from '../../assets/images/mohan-muruge.jpg';
+import mohan from '../../assets/images/mohan-muruge.jpg';
 
 function Comments(props) {
     return (
         <section className="comments">
             <h2 className="comments__title">3 Comments</h2>
-            <form className="comments__form">
-                <img className="comments__image" src={Mohan} width={48} height={48} alt=""/>
-                <div>
-                    <div className="comments__wrapper">
-                        <label className="comments__label">JOIN THE CONVERSATION</label>
-                        <textarea className="comments__input" value="Write comment here" readOnly></textarea>
-                    </div>
-                    <button className="button__comment">COMMENT</button>
-                </div>
-            </form>
+            <div className="comments__new">
+                <img className="comments__image" src={mohan} width={48} height={48} alt=""/>
+                <form className="comments__form">
+                        <div className="comments__entry">
+                            <label className="comments__label">JOIN THE CONVERSATION</label>
+                            <textarea className="comments__input" placeholder="Write comment here"></textarea>
+                        </div>
+                        <button className="button comment">COMMENT</button>
+                </form>
+            </div>
         {props.comments.map((comment) => 
-            <div className="comments__card" key={comment.timestamp}>
+            <div className="comments__cards" key={comment.timestamp}>
                 <img className="card__image" alt=""/>
-                <div className="card__wrapper">
+                <div className="card__info">
                     <div className="card__header">
-                        <h4 className="card__name">{comment.name}</h4>
-                        <h5 className="card__timestamp">{props.convertTime(comment.timestamp)}</h5>
+                        <span className="card__name">{comment.name}</span>
+                        <span className="card__timestamp">{props.convertTime(comment.timestamp)}</span>
                     </div>
                     <p className="card__comment">{comment.comment}</p>
                 </div>
