e46aa369697ab8811c198a798e73c96ba9570aee
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-08 13:25:36 -0500

    Create NextVideo component and move map function into that compoenent as well as required styling from Aside.scss

--- Sprint 1/src/components/Aside/Aside.js
+++ Sprint 1/src/components/Aside/Aside.js
@@ -1,18 +1,11 @@
+import NextVideo from '../NextVideo/NextVideo'
 import './Aside.scss'
 
 function Aside({videoList}) {
     return (
         <aside className="next-video">
             <h3 className="next-video__title">NEXT VIDEO</h3>
-        {videoList.filter(video => video.id!=='1').map((video) =>
-            <div className="next-video__cards" key={video.id}>
-                <img className="card__image-next" src={video.image} alt=""/>
-                <div className="card__info-next">
-                    <span className="card__title-next">{video.title}</span>
-                    <span className="card__channel-next">{video.channel}</span>
-                </div>
-            </div>
-        )}
+            <NextVideo videoList={videoList}/>
         </aside>
     )
 };
