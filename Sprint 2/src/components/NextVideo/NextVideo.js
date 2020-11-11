9c1410b1d9baabab251ab42354a10b7368122d02
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-11 16:21:16 -0500

    Add apiKey.js and change span to p in next video section

--- Sprint 2/src/components/NextVideo/NextVideo.js
+++ Sprint 2/src/components/NextVideo/NextVideo.js
@@ -7,8 +7,8 @@ function NextVideo({videoList}) {
             <div className="next-video__card" key={video.id}>
                 <img className="card__image-next" src={video.image} alt=""/>
                 <div className="card__info-next">
-                    <span className="card__title-next">{video.title}</span>
-                    <span className="card__channel-next">{video.channel}</span>
+                    <p className="card__title-next">{video.title}</p>
+                    <p className="card__channel-next">{video.channel}</p>
                 </div>
             </div>
         )}
