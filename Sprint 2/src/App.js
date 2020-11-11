31c20dc785ae7b03087854561f8f916cc53af490
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-11 12:31:45 -0500

    Made Home a class compoenent to hold state. Placed App.js content inside Home and imported home into App.js

--- Sprint 2/src/App.js
+++ Sprint 2/src/App.js
@@ -1,37 +1,13 @@
 import React from 'react';
 import './App.scss';
-import { mainVideo, videoList } from './utils/videoData';
-import Header from './components/Header/Header';
-import VideoBody from './components/VideoBody/VideoBody';
+import Home from './pages/Home/Home';
 
 class App extends React.Component {
-	state = {
-		mainVideo: mainVideo,
-		videoList: videoList
-	};
-
-	handleSearchSubmit = (e, form) => {
-		e.preventDefault();
-		form.reset();
-	};
-
-	handleCommentSubmit = (e, form) => {
-		e.preventDefault();
-		form.reset();
-	};
 
 	render() {
 		return (
 			<>
-				<Header 
-					mainVideo={this.state.mainVideo}
-					onSearchClick={this.handleSearchSubmit}
-				/>
-				<VideoBody 
-					mainVideo={this.state.mainVideo} 
-					videoList={this.state.videoList}
-					onCommentClick={this.handleCommentSubmit}
-				/>
+				<Home />		
 			</>
 		)
 	};
