e1933c0f6ec2489edcf428d2ac06db030752a366
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-11 12:44:54 -0500

    Add JSX html structure to Upload page and import required assests. Import upload to App in order to test render.

--- Sprint 2/src/App.js
+++ Sprint 2/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.scss';
 import Home from './pages/Home/Home';
+import Upload from './pages/Upload/Upload'
 
 class App extends React.Component {
 
 	render() {
 		return (
 			<>
-				<Home />		
+				<Home />
+				<Upload />		
 			</>
 		)
 	};
--- Sprint 2/src/pages/Upload/Upload.js
+++ Sprint 2/src/pages/Upload/Upload.js
@@ -1,1 +1,37 @@
 import './Upload.scss';
+import React from 'react';
+import SearchBar from '../../components/SearchBar/SearchBar'
+import uploadImage from '../../assets/images/upload-video-preview.jpg'
+
+function Upload(props) {
+    return (
+        <div className="upload-page">
+            <SearchBar/>
+            <div className="upload">
+                <h1>Upload Video</h1>
+                <form>
+                    <div>
+                        <label>VIDEO THUMBNAIL</label>
+                        <img src={uploadImage} alt=""/>
+                    </div>
+                    <div>
+                        <div>
+                            <label>TITLE YOUR VIDEO</label>
+                            <input placeholder="Add a title to your video"/>
+                        </div>
+                        <div>
+                            <label>ADD A VIDEO DESCRIPTION</label>
+                            <textarea placeholder="Add a description of your video"></textarea>
+                        </div>
+                    </div>
+                </form>
+                <div>
+                    <button>CANCEL</button>
+                    <button>PUBLISH</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Upload;
