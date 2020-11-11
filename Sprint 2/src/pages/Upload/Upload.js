875a69c9b0d0a3d5e4c855d1f4463221a0705686
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-11 13:55:31 -0500

    Change upload class name to uploads after running into styling error with upload button. Add initial styling for mobile and tablet to replicate mockups. Simple margin padding adjustments and including common styling and mixins with slight modifications.

--- Sprint 2/src/pages/Upload/Upload.js
+++ Sprint 2/src/pages/Upload/Upload.js
@@ -5,29 +5,29 @@ import uploadImage from '../../assets/images/upload-video-preview.jpg'
 
 function Upload(props) {
     return (
-        <div className="upload-page">
+        <div className="uploads-page">
             <SearchBar/>
-            <div className="upload">
-                <h1>Upload Video</h1>
-                <form>
-                    <div>
-                        <label>VIDEO THUMBNAIL</label>
-                        <img src={uploadImage} alt=""/>
+            <div className="uploads">
+                <h1 className="uploads__title">Upload Video</h1>
+                <form className="uploads__form">
+                    <div className="thumbnail">
+                        <label className="thumbnail__label">VIDEO THUMBNAIL</label>
+                        <img className="thumbnail__image" src={uploadImage} alt=""/>
                     </div>
-                    <div>
-                        <div>
-                            <label>TITLE YOUR VIDEO</label>
-                            <input placeholder="Add a title to your video"/>
+                    <div className="uploads__info">
+                        <div className="title">
+                            <label className="title__label">TITLE YOUR VIDEO</label>
+                            <input className="title__input" placeholder="Add a title to your video"/>
                         </div>
-                        <div>
-                            <label>ADD A VIDEO DESCRIPTION</label>
-                            <textarea placeholder="Add a description of your video"></textarea>
+                        <div className="description">
+                            <label className="description__label">ADD A VIDEO DESCRIPTION</label>
+                            <textarea className="description__input" placeholder="Add a description of your video"></textarea>
                         </div>
                     </div>
                 </form>
-                <div>
-                    <button>CANCEL</button>
-                    <button>PUBLISH</button>
+                <div className="uploads__buttons">
+                    <button className="button cancel">CANCEL</button>
+                    <button className="button publish">PUBLISH</button>
                 </div>
             </div>
         </div>
