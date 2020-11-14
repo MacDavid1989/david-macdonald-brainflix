78c5a79032472d39deb13e08e77ad91a18e04391
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-13 21:01:49 -0500

    Add conditional for componenetDidUpdate to check for url match which will call a get if they are different. Remove filter from NextVideo and add Link to change between videos.

--- Sprint 2/src/components/NextVideo/NextVideo.js
+++ Sprint 2/src/components/NextVideo/NextVideo.js
@@ -1,16 +1,19 @@
 import './NextVideo.scss'
+import {Link} from 'react-router-dom'
 
 function NextVideo({videoList}) {
     return (
         <>
-        {videoList.filter(video => video.id!=='1').map((video) =>
-            <div className="next-video__card" key={video.id}>
-                <img className="card__image-next" src={video.image} alt=""/>
-                <div className="card__info-next">
-                    <p className="card__title-next">{video.title}</p>
-                    <p className="card__channel-next">{video.channel}</p>
+        {videoList.map((video) =>
+            <Link to={`/${video.id}`}>
+                <div className="next-video__card" id={video.id} key={video.id}>
+                    <img className="card__image-next" src={video.image} alt=""/>
+                    <div className="card__info-next">
+                        <p className="card__title-next">{video.title}</p>
+                        <p className="card__channel-next">{video.channel}</p>
+                    </div>
                 </div>
-            </div>
+            </Link>
         )}
         </>
     )
--- Sprint 2/src/pages/Home/Home.js
+++ Sprint 2/src/pages/Home/Home.js
@@ -15,7 +15,6 @@ class Home extends Component {
 	componentDidMount() {
 
 		if(!this.state.mainVideo){
-			console.log('hey')
 			axios.get(`https://project-2-api.herokuapp.com/videos/1af0jruup5gu${API_KEY}`)
 			.then(mainVideo => {
 				axios.get(`https://project-2-api.herokuapp.com/videos${API_KEY}`)
@@ -24,6 +23,27 @@ class Home extends Component {
 						{
 							mainVideo: mainVideo.data, 
 							videoList: videoList.data.filter(video => video.id !== '1af0jruup5gu')
+						})
+				})
+				.catch(console.error)
+			})
+			.catch(console.error)	
+		}
+
+		console.log(this.props.match)
+	}
+
+	componentDidUpdate() {
+
+		if(this.props.match.url !== '/'){
+			axios.get(`https://project-2-api.herokuapp.com/videos/${this.props.match.params.id + API_KEY}`)
+			.then(mainVideo => {
+				axios.get(`https://project-2-api.herokuapp.com/videos${API_KEY}`)
+				.then(videoList => {
+					this.setState(
+						{
+							mainVideo: mainVideo.data, 
+							videoList: videoList.data.filter(video => video.id !== this.props.match.params.id)
 						})
 				})
 				.catch(console.error)
