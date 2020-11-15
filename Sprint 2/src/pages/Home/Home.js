8e73f3084a912eacfe2737326e8f5f3872d1cae3
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-14 21:28:59 -0500

    Add comments and refactor lifecycle methods found in Home component

--- Sprint 2/src/pages/Home/Home.js
+++ Sprint 2/src/pages/Home/Home.js
@@ -14,69 +14,65 @@ class Home extends Component {
 		videoList: []
 	};
 
+	// lifecycle method that is called after the component first renders
 	componentDidMount() {
+
+		// check in place to see if the current route is the home path 
 		if(this.props.match.url === '/'){
+
+			// get request for default video utilizing default video id
 			axios.get(`https://project-2-api.herokuapp.com/videos/${defaultVideoId + API_KEY}`)
-			.then(mainVideo => {
-				this.setState({mainVideo: mainVideo.data})
-			})
-			.catch(console.error)	
-			axios.get(`https://project-2-api.herokuapp.com/videos${API_KEY}`)
-			.then(videoList => {
-				this.setState(
-					{videoList: videoList.data.filter(video => video.id !== defaultVideoId)})
-			})
+			// after successful response, changes state to hold default video matching default id
+			.then(mainVideo => this.setState({mainVideo: mainVideo.data}))
 			.catch(console.error)
-		} else {
-			axios.get(`https://project-2-api.herokuapp.com/videos/${this.props.match.params.id  + API_KEY}`)
-			.then(mainVideo => {
-				this.setState({mainVideo: mainVideo.data})
-			})
-			.catch(console.error)	
+			
+			// get request for video list
 			axios.get(`https://project-2-api.herokuapp.com/videos${API_KEY}`)
-			.then(videoList => {
-				this.setState(
-					{
-						videoList: videoList.data.filter(video => video.id !== this.props.match.params.id)
-					})
-			})
+			// after successful response, changes state to hold a video list with all videos that do not match the default video id
+			.then(videoList => this.setState({videoList: videoList.data.filter(video => video.id !== defaultVideoId)}))
+			.catch(console.error)
+
+		} else {
+
+			// get request for video matching the video id contained within the match route prop params object id key
+			axios.get(`https://project-2-api.herokuapp.com/videos/${this.props.match.params.id  + API_KEY}`)
+			.then(mainVideo => this.setState({mainVideo: mainVideo.data}))
+			.catch(console.error)	
+
+			// get request for video list
+			axios.get(`https://project-2-api.herokuapp.com/videos${API_KEY}`)
+			// after successful response, changes state to hold a video list with all videos that do not match the match route prop params object id key
+			.then(videoList => this.setState({videoList: videoList.data.filter(video => video.id !== this.props.match.params.id)}))
 			.catch(console.error)
 		}
 	}
 
+	// lifecycle method that is called after the component is updated
 	componentDidUpdate(prevProps) {
 
+		// check in place to see if there was a change in the url id and that the current id is not undefined
 		if(this.props.match.params.id !== prevProps.match.params.id && this.props.match.params.id !== undefined){
+			
+			// similar get requests as found in the DidMount method to update state with the correct data
 			axios.get(`https://project-2-api.herokuapp.com/videos/${this.props.match.params.id  + API_KEY}`)
-			.then(mainVideo => {
-				this.setState({
-					mainVideo: mainVideo.data
-				})
-			})
+			.then(mainVideo => this.setState({mainVideo: mainVideo.data}))
 			.catch(console.error)	
+
 			axios.get(`https://project-2-api.herokuapp.com/videos${API_KEY}`)
-			.then(videoList => {
-				this.setState(
-					{
-						videoList: videoList.data.filter(video => video.id !== this.props.match.params.id)
-					})
-			})
+			.then(videoList => this.setState({videoList: videoList.data.filter(video => video.id !== this.props.match.params.id)}))
 			.catch(console.error)	
-		} else if(this.props.match.params.id !== prevProps.match.params.id && this.props.match.params.id === undefined) {
+
+		}
+		// alternate condition to check that the id key is undefined which is the case for the home page route
+		else if(this.props.match.params.id !== prevProps.match.params.id && this.props.match.params.id === undefined) {
+			
+			// similar get requests as found in the DidMount method to update state with the correct data
 			axios.get(`https://project-2-api.herokuapp.com/videos/1af0jruup5gu${API_KEY}`)
-			.then(mainVideo => {
-				this.setState({
-					mainVideo: mainVideo.data
-				})
-			})
+			.then(mainVideo => this.setState({mainVideo: mainVideo.data}))
 			.catch(console.error)	
+			
 			axios.get(`https://project-2-api.herokuapp.com/videos${API_KEY}`)
-			.then(videoList => {
-				this.setState(
-					{
-						videoList: videoList.data.filter(video => video.id !== '1af0jruup5gu')
-					})
-			})
+			.then(videoList => this.setState({videoList: videoList.data.filter(video => video.id !== defaultVideoId)}))
 			.catch(console.error)
 		}
 	}
