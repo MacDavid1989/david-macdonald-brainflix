2035b4b5b3ab1e3022af41a92cfc0fcad23c4118
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-14 22:34:17 -0500

    Add comments to video body and corresponding scss file

--- Sprint 2/src/components/Main/Main.js
+++ Sprint 2/src/components/Main/Main.js
@@ -3,7 +3,7 @@ import convertTime from '../../utils/convertTime';
 import Article from "../Article/Article";
 import VideoComments from "../VideoComments/VideoComments";
 
-function Main({history, match, mainVideo, onComment, onDelete}) {
+function Main({match, mainVideo, onComment, onDelete}) {
 
     return (
         <main className="main">
@@ -12,7 +12,6 @@ function Main({history, match, mainVideo, onComment, onDelete}) {
                 convertTime={convertTime}
             />
             <VideoComments 
-                history={history}
                 match={match} 
                 comments={mainVideo.comments}
                 onComment={onComment}
--- Sprint 2/src/components/VideoBody/VideoBody.js
+++ Sprint 2/src/components/VideoBody/VideoBody.js
@@ -2,13 +2,12 @@ import './VideoBody.scss';
 import Main from '../Main/Main';
 import Aside from '../Aside/Aside';
 
-
-function VideoBody({history, match, mainVideo, videoList, onComment, onDelete}) {
+// wrapper component for components mounted beneath video player
+function VideoBody({match, mainVideo, videoList, onComment, onDelete}) {
     
     return (
         <div className="video__body">
             <Main
-                history={history}
                 match={match} 
                 mainVideo={mainVideo}
                 onComment={onComment}
