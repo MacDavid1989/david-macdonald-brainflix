4ddf3b991abb2eb03d010734bff8239088bb8a5b
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-14 22:41:33 -0500

    Add comments to article component and scss where BEM and labels are not sufficiently descriptive

--- Sprint 2/src/components/Article/Article.js
+++ Sprint 2/src/components/Article/Article.js
@@ -5,13 +5,17 @@ import views from '../../assets/icons/svg/icon-views.svg';
 function Article({mainVideo, convertTime}) {
 
     return (
+        // wrapper for all information associated with current main video stored in state
         <article className="video-article">
+            {/* video title */}
             <h1 className="video-article__title">{mainVideo.title}</h1>
             <div className="video-article__info">
+                {/* video owner information including timestamp and channel */}
                 <div className="owner">
                     <span className="owner__channel">{mainVideo.channel}</span>
                     <span className="owner__timestamp">{convertTime(mainVideo.timestamp)}</span>
                 </div>
+                {/* video like and views wrapper */}
                 <div className="video-article__stats">
                     <div className="views">
                         <img className="views__icon" src={views} alt=""/>
