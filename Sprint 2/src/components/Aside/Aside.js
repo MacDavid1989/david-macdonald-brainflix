bdfcb9574b07acded0a06f61e9cd9da232b3ad27
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-14 23:18:42 -0500

    Add comments to aside component and scss

--- Sprint 2/src/components/Aside/Aside.js
+++ Sprint 2/src/components/Aside/Aside.js
@@ -1,6 +1,7 @@
 import './Aside.scss'
 import NextVideo from '../NextVideo/NextVideo'
 
+// aisde component to separate next video list from main body
 function Aside({videoList}) {
     return (
         <aside className="next-video">
