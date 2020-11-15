5fdbd0c9518817568206f732777320eb0cf0b394
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-14 22:07:54 -0500

    Add comments to searchbar component

--- Sprint 2/src/components/SearchBar/SearchBar.js
+++ Sprint 2/src/components/SearchBar/SearchBar.js
@@ -4,15 +4,20 @@ import logo from '../../assets/logo/logo-brainflix.svg';
 import upload from '../../assets/icons/svg/icon-upload.svg';
 import mohan from '../../assets/images/mohan-muruge.jpg';
 
+// SearchBar component
 function SearchBar(props) {
     return (
         <section className="search-bar">
+            {/* Router link to act as anchor tag and link logo to Home component via the specified path */}
             <Link to="/">
                 <img className="search-bar__logo" src={logo} alt=""/>
             </Link>
+            {/* search bar form */}
             <div className="search-bar__form">
+                {/* search input field */}
                 <input type="search" name="searchBarInput" className="search-bar__input" placeholder="Search"></input>
                 <div className="search-bar__wrapper">
+                    {/* Router link to anchor upload button to upload component via the /upload route path */}
                     <Link to="/upload" className="link-upload">
                         <button className="button upload">
                             <img className="upload__icon" src={upload} alt=""/>
