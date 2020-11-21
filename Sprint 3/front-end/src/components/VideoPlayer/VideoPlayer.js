7ad5ca00fd508dd791c4644fe81fa8e090c7cfc6
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-20 22:51:40 -0500

    Add toggle to video itself for playback. Add function tu update elapsed time and two functions to gather data on video load and then use that function to set attribute of progress bar. The second function will set value of progress to time of video.

--- Sprint 3/front-end/src/components/VideoPlayer/VideoPlayer.js
+++ Sprint 3/front-end/src/components/VideoPlayer/VideoPlayer.js
@@ -13,36 +13,63 @@ class VideoPlayer extends Component {
         this.video = React.createRef();
         this.playbackPlay = React.createRef();
         this.playbackPause = React.createRef();
-      }
+        this.elapsed = React.createRef();
+        this.duration = React.createRef();
+        this.progress = React.createRef();
+    }
 
-      togglePlay = () => {
-          const video = this.video.current
-          if (video.paused || video.ended) {
-              this.updatePlayButton()
-            return video.play();
-          } else {
+    togglePlay = () => {
+        const video = this.video.current
+        if (video.paused || video.ended) {
             this.updatePlayButton()
-
+            return video.play();
+        } else {
+            this.updatePlayButton()
             return video.pause();
-          }
         }
+    }
 
-        updatePlayButton = () =>{
-            const playbackPlay = this.playbackPlay.current;
-            const playbackPause = this.playbackPause.current;
-            playbackPlay.classList.toggle('hidden')
-            playbackPause.classList.toggle('hidden')
-        }
+    updatePlayButton = () =>{
+        const playbackPlay = this.playbackPlay.current;
+        const playbackPause = this.playbackPause.current;
+        playbackPlay.classList.toggle('hidden')
+        playbackPause.classList.toggle('hidden')
+    }
+
+   formatTime = (timeInSeconds) => {
+        const result = new Date(timeInSeconds * 1000).toISOString().substr(11, 8);
+      
+        return {
+          minutes: result.substr(3, 2),
+          seconds: result.substr(6, 2),
+        };
+      };
+
+    updateTimeElapsed= () => {
+        const video = this.video.current
+        const timeElapsed = this.elapsed.current
+        const time = this.formatTime(Math.round(video.currentTime));
+        timeElapsed.innerText = `${time.minutes}:${time.seconds}`;
+        timeElapsed.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+    }
+
+    initializedVideo = () => {
+        const videoDuration = Math.round(this.video.current.duration);
+        this.progress.current.setAttribute('max', videoDuration);
+    }
+
+    updateProgress =() => {
+        this.progress.current.value = Math.floor(this.video.current.currentTime);
+    }
 
     render() {
-
         return (
             // wrapper for video player
             <header className="header">
                 {/* video player */}
                 <section className="player">
                     {/* video source and text if unsupported */}
-                    <video ref={this.video} className="video" id="video" preload="metadata" poster={this.props.mainVideo.image}>
+                    <video onClick={this.togglePlay} onLoadedMetadata={this.initializedVideo} onTimeUpdate={()=>{this.updateTimeElapsed(); this.updateProgress()}} ref={this.video} className="video" id="video" preload="metadata" poster={this.props.mainVideo.image}>
                         <source src={this.props.mainVideo.video} type="video/mp4"></source>
                         <p className="video__text">Your browser doesn't support HTML5 video.</p> 
                     </video>
@@ -54,11 +81,11 @@ class VideoPlayer extends Component {
                         </button>
                         {/* video progress bar and time */}
                         <div className="progress">
-                            <progress className="progress__bar" max="100" value="0"></progress>
+                            <progress ref={this.progress} className="progress__bar" value="0" min="0"></progress>
                             <div className="time">
-                                <time className="time__elapsed">0:00</time>
+                                <time ref={this.elapsed} className="time__elapsed">0:00</time>
                                 <span className="time__separation">/</span>
-                                <time className="time__total">{this.props.mainVideo.duration}</time>
+                                <time ref={this.duration} className="time__total">{this.props.mainVideo.duration}</time>
                             </div>
                         </div>
                         {/* right video controls */}
