0b19b3d8eb70f9ef8b35b2a08b33e1fe64986ae6
David <62397679+MacDavid1989@users.noreply.github.com>
2020-11-22 15:04:14 -0500

    Add comment to viewsRoute in back end

--- Sprint 3/back-end/routes/viewsRoute.js
+++ Sprint 3/back-end/routes/viewsRoute.js
@@ -6,6 +6,7 @@ const mainVideosFile = './data/mainVideos.json';
 router.put('/', (req, res) => {
     const data = fs.readFileSync(mainVideosFile)
     const parsedData = JSON.parse(data)
+    // increment the views of the video that matches the id passed in the put request
     parsedData.find(video => video.id === req.videoInfo.videoId).views++
     // overwrite mainVideos.json to update with the video like
     fs.writeFile('./data/mainVideos.json', JSON.stringify([...parsedData]), (err) => console.log('this is the error :', err))
